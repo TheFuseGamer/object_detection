7567461d740114d58ef83e20039ce9068849ffb4
Amir Halloul <amirhalloul@gmail.com>
2019-03-28 17:51:30 +0100

    Changed dataset

--- resize_images.py
+++ resize_images.py
@@ -29,14 +29,21 @@ if __name__ == "__main__":
         default="(800, 600)",
         type=str,
     )
+    parser.add_argument(
+        "--startingn",
+        help="Starting number to label images.",
+        default="0",
+        type=int,
+    )
     args = parser.parse_args()
 
     raw_dir = args.raw_dir
     save_dir = args.save_dir
     ext = args.ext
-    target_size = eval(args.target_size)
+    target_size = eval(args.target_size)	
     msg = "--target-size must be a tuple of 2 integers"
     assert isinstance(target_size, tuple) and len(target_size) == 2, msg
+    startingn = args.startingn
     fnames = glob.glob(os.path.join(raw_dir, "*.{}".format(ext)))
     os.makedirs(save_dir, exist_ok=True)
     print(
@@ -48,7 +55,7 @@ if __name__ == "__main__":
         print(".", end="", flush=True)
         img = cv2.imread(fname)
         img_small = cv2.resize(img, target_size)
-        new_fname = "{}.{}".format(str(i), ext)
+        new_fname = "{}.{}".format(str(startingn + i), ext)
         small_fname = os.path.join(save_dir, new_fname)
         cv2.imwrite(small_fname, img_small)
     print(
